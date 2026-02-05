7ea0c5e69d38004adcec14cc5044cb81baab5b2a
Jerikoy <75771841+Jerikoy20@users.noreply.github.com>
2026-02-05 09:48:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // 🔴 PASTE YOUR REAL MONGODB LINK HERE
 // (Make sure to replace <password> and check the cluster address!)
-const MONGO_URI = "mongodb+srv://jerikoykoy_db_user:admin2026@cluster0.wahy7zh.mongodb.net/?appName=Cluster0";
+const MONGO_URI = "mongodb+srv://jerikoykoy_db_user:bims2026@cluster0.wahy7zh.mongodb.net/?appName=Cluster0";
 
 app.use(cors());
 app.use(express.json());
@@ -115,7 +115,7 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-// --- START SERVER (Only Once!) ---
+// --- START SERVER ---
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
